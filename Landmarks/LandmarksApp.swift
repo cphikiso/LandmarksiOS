a50085545a99e7a97c7797d9200570def2889f6c
Chimwemwe Phikiso <97michelangelo@gmail.com>
2022-06-10 18:31:02 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chimwemwe Phikiso on 10/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
