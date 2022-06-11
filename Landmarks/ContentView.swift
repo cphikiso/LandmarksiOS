03407e87abcc334a9597846b6cbe7f1c4e7a63b1
Chimwemwe Phikiso <97michelangelo@gmail.com>
2022-06-11 01:38:38 +0100

    Creating and Combining Views

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+                
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle")
+                    .font(.title).bold()
+                HStack {
+                    Text("Joshua Tree National Park")
+                       
+                    Spacer()
+                    Text("California")
+                        
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About The Forest Days")
+                    .font(.title2)
+            }
             .padding()
+            Spacer()
+                
+        }
+            
     }
 }
 
