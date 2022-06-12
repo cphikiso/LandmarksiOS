ed4d3a40a738223b6c261dcc6a6168c97a9ba23e
Chimwemwe Phikiso <97michelangelo@gmail.com>
2022-06-12 02:26:57 +0100

    add unversioned files

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,35 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-                
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            VStack(alignment: .leading) {
-                Text("Turtle")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                       
-                    Spacer()
-                    Text("California")
-                        
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                Divider()
-                Text("About The Forest Days")
-                    .font(.title2)
-            }
-            .padding()
-            Spacer()
-                
-        }
-            
+        LandmarksList()
     }
 }
 
